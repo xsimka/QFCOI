89883949bae0d5119af3ea860ac78e5e87268541
xsimka <45574565+xsimka@users.noreply.github.com>
2025-05-22 12:02:42 +0200

    Update Example_code.m

--- Example_usage/Example_code.m
+++ Example_usage/Example_code.m
@@ -1,6 +1,7 @@
 close all
 clear all
 
+% Images are drawn from the OMNIQAD database available here: {https://zenodo.org/records/7607071}
 ref_image = imread('ref_Telescope.bmp');
 test_image = imread('Telescope46AVIF.bmp');
 
