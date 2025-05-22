35e4505560e1a5c2af66d62edff90b74cbdbde85
xsimka <45574565+xsimka@users.noreply.github.com>
2025-05-22 11:54:11 +0200

    Update Example_code.m

--- Example_usage/Example_code.m
+++ Example_usage/Example_code.m
@@ -1,8 +1,8 @@
 close all
 clear all
 
-ref_image = imread('ref_Flowers.bmp');
-test_image = imread('Flowers51AVIF.bmp');
+ref_image = imread('ref_Telescope.bmp');
+test_image = imread('Telescope46AVIF.bmp');
 
 
 result = QFCOI(ref_image,test_image)
